67cfde592b93de15667ad684ddb9e4dcbf836d8e
Areeba Shah <your-email@example.com>
2025-05-21 05:44:59 -0700

    -

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-
+// hamburger script-------
     const hamburger = document.getElementById("hamburger");
     const navLinks = document.getElementById("nav-links");
 
@@ -6,4 +6,27 @@
       hamburger.classList.toggle("active");
       navLinks.classList.toggle("active");
     });
- 
+
+
+    
+
+// ---bar filling script-----
+     document.addEventListener("DOMContentLoaded", function () {
+    const observer = new IntersectionObserver((entries, observer) => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          document.querySelectorAll('.fill').forEach(fill => {
+            fill.classList.add('animate');
+          });
+          observer.unobserve(entry.target);
+        }
+      });
+    }, {
+      threshold: 0.3
+    });
+
+    const accuracySection = document.querySelector('.accuracy');
+    if (accuracySection) {
+      observer.observe(accuracySection);
+    }
+  });
